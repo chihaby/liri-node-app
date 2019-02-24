5c2b33fe706f17da96887bc989d437959d091189
Rad <chihaby@gmail.com>
2019-02-24 11:58:39 -0800

    adds band file

---
+++ bands.js
@@ -0,0 +1,16 @@
+var axios = require("axios");
+var open = require("open");
+var moment = require("moment");
+
+var artist = process.argv[2];
+
+var bands = function (artist) {
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
+        function (response) {
+            //open(response);
+            console.log(response.Artist);
+        })
+}
+
+module.exports = bands(artist);
+
--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var play = require("./spotify.js");
 var movie = require("./movie.js");
+var bands = require("./bands.js");
 
 play;
 movie;
+band;
